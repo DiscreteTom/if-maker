1aa7d60ec031558e2af25b2008244b10a2318484
DiscreteTom <discrete_tom@outlook.com>
2019-05-19 21:08:12 +0800

    init Data.py

---
+++ src/Data.py
@@ -0,0 +1,16 @@
+import yaml
+
+class Data:
+	config = {}
+	items = []
+	def __init__(self):
+		# load config
+		f = open('_config.yml', encoding='utf-8')
+		self.config = yaml.load(f)
+		f.close()
+		# load items
+		f = open('_items.yml', encoding='utf-8')
+		self.items = yaml.load(f)
+		f.close()
+
+data = Data()
