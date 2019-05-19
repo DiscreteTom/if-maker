336c35945a244687435cabf3c179f0e604bdd45c
DiscreteTom <discrete_tom@outlook.com>
2019-05-19 23:12:14 +0800

    rename data.py(2/2), use safe_load for yaml

---
+++ src/data.py
@@ -0,0 +1,20 @@
+'''
+use `data.config` to access config file, use `data.items` to access items file
+'''
+
+import yaml
+
+class Data:
+	config = {}
+	items = {}
+	def __init__(self):
+		# load config
+		f = open('../_config.yml', encoding='utf-8')
+		self.config = yaml.safe_load(f)
+		f.close()
+		# load items
+		f = open('../_items.yml', encoding='utf-8')
+		self.items = yaml.safe_load(f)
+		f.close()
+
+data = Data()
