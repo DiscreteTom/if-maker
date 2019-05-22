fb4a73b3909c82cf97578f62f4190e449d57254e
DiscreteTom <discrete_tom@outlook.com>
2019-05-22 23:51:45 +0800

    update data.py to adapt refdict

--- src/data.py
+++ src/data.py
@@ -2,20 +2,21 @@
 use `data.config` to access config file, use `data.items` to access items file
 '''
 
+from refdict import refdict
 import yaml
 
 class Data:
 	def __init__(self):
 		# load config
 		f = open('../_config.yml', encoding='utf-8')
-		self.config = yaml.safe_load(f)
+		self.config = refdict(yaml.safe_load(f))
 		f.close()
 		# load items
 		f = open('../_items.yml', encoding='utf-8')
-		self.items = yaml.safe_load(f)
+		self.items = refdict(yaml.safe_load(f))
 		f.close()
 		# user defined global data
-		self.globalData = {}
+		self.globalData = refdict({})
 	def findItem(self, itemName: str):
 		'''
 		return itemID
