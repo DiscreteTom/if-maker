96ab42936d3e8f303137a062d20f36da0b465b99
DiscreteTom <discrete_tom@outlook.com>
2019-05-28 15:16:36 +0800

    update translator.py finish translator.run() and translator.preprocess() simply

--- src/translator.py
+++ src/translator.py
@@ -1,12 +1,23 @@
-from shell import shell
-from data import data
-
 class translator:
 	@classmethod
-	def run(cls, code: str):
-		lines = code.split(';')
-		result = None
-		for line in lines:
-			if len(line.strip()):
-				result = eval(line.strip())
+	def run(cls, code: str, params = {}):
+		from shell import shell
+		from data import data
+		cmds = code.split(';')
+		for cmd in cmds:
+			cmd = cmd.strip()
+			if len(cmd) == 0:
+				# empty command
+				continue
+			result = eval(preprocess(cmd), globals(), params)
 		return result
+
+	@classmethod
+	def preprocess(cls, cmd: str):
+		if cmd.startswith('load(') or cmd.startswith('unload(') or cmd.startswith('parse('):
+			return 'shell.' + cmd
+		if cmd.startswith('print(') or cmd.startswith('printStory('):
+			return 'story.' + cmd
+		if cmd.startswith('findItem('):
+			return 'data.' + cmd
+		return cmd
