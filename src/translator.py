c2f05de875fb1e21dc18dce512a163425b55ad3e
DiscreteTom <discrete_tom@outlook.com>
2019-05-28 16:31:56 +0800

    cancel class translator

--- src/translator.py
+++ src/translator.py
@@ -1,23 +1,20 @@
-class translator:
-	@classmethod
-	def run(cls, code: str, params = {}):
-		from shell import shell
-		from data import data
-		cmds = code.split(';')
-		for cmd in cmds:
-			cmd = cmd.strip()
-			if len(cmd) == 0:
-				# empty command
-				continue
-			result = eval(preprocess(cmd), globals(), params)
-		return result
+def run(code: str, params = {}):
+	from shell import shell
+	from data import data
+	cmds = code.split(';')
+	for cmd in cmds:
+		cmd = cmd.strip()
+		if len(cmd) == 0:
+			# empty command
+			continue
+		result = eval(preprocess(cmd), globals(), params)
+	return result
 
-	@classmethod
-	def preprocess(cls, cmd: str):
-		if cmd.startswith('load(') or cmd.startswith('unload(') or cmd.startswith('parse('):
-			return 'shell.' + cmd
-		if cmd.startswith('print(') or cmd.startswith('printStory('):
-			return 'story.' + cmd
-		if cmd.startswith('findItem('):
-			return 'data.' + cmd
-		return cmd
+def preprocess(cmd: str):
+	if cmd.startswith('load(') or cmd.startswith('unload(') or cmd.startswith('parse('):
+		return 'shell.' + cmd
+	if cmd.startswith('print(') or cmd.startswith('printStory('):
+		return 'story.' + cmd
+	if cmd.startswith('findItem('):
+		return 'data.' + cmd
+	return cmd
