70446bbad64079de0891a99ef92c2501add81a68
DiscreteTom <discrete_tom@outlook.com>
2019-05-28 16:28:44 +0800

    update controller.py cancel class Controller

--- src/controller.py
+++ src/controller.py
@@ -6,43 +6,40 @@ from data import data
 from shell import shell
 from story import story
 
-class Controller:
-	def start(self):
-		# print welcome
-		if 'welcome' in data.config['project']:
-			print(data.config['project']['welcome'])
-		else:
-			print(data.config['project']['name'])
+def start():
+	# print welcome
+	if 'welcome' in data.config['project']:
+		print(data.config['project']['welcome'])
+	else:
+		print(data.config['project']['name'])
 
-		# print main menu
-		for i in range(len(data.config['mainMenu'])):
-			print(i + 1, data.config['mainMenu'][i])
+	# print main menu
+	for i in range(len(data.config['mainMenu'])):
+		print(i + 1, data.config['mainMenu'][i])
 
-		# get user's choice
-		index = 0
-		while True:
-			index = ord(msvcrt.getch()) - ord('0')
-			if index in range(1, len(data.config['mainMenu']) + 1):
-				break
+	# get user's choice
+	index = 0
+	while True:
+		index = ord(msvcrt.getch()) - ord('0')
+		if index in range(1, len(data.config['mainMenu']) + 1):
+			break
 
-		# os.system('cls') # clear the console
+	# judge user input
+	if data.config['mainMenu'][index - 1] == 'new':
+		return newGame()
+	elif data.config['mainMenu'][index - 1] == 'continue':
+		return loop()
+	elif data.config['mainMenu'][index - 1] == 'exit':
+		return 0
 
-		# judge user input
-		if data.config['mainMenu'][index - 1] == 'new':
-			return self.newGame()
-		elif data.config['mainMenu'][index - 1] == 'continue':
-			return self.loop()
-		elif data.config['mainMenu'][index - 1] == 'exit':
-			return 0
+def newGame():
+	story.printStory(0)
+	loop()
 
-	def newGame(self):
-		story.tell(0)
-		self.loop()
-
-	def loop(self):
-		while True:
-			s = input(data.config['system']['shell']['prefix'])
-			if s == data.config['system']['shell']['exitCmd']:
-				break
-			if shell.parse(s) == False:
-				print(data.config['system']['shell']['errorMsg'])
+def loop():
+	while True:
+		s = input(data.config['system']['shell']['prefix'])
+		if s == data.config['system']['shell']['exitCmd']:
+			break
+		if shell.parse(s) == False:
+			print(data.config['system']['shell']['errorMsg'])
