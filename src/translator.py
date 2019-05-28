7b7bf38765b3531bf790694e49b229e116b2a9bf
DiscreteTom <discrete_tom@outlook.com>
2019-05-28 17:58:20 +0800

    update translator.py, add local params

--- src/translator.py
+++ src/translator.py
@@ -1,12 +1,17 @@
 def run(code: str, params = {}):
+	print(code)
 	from shell import shell
 	from data import data
+	from story import story
 	cmds = code.split(';')
 	for cmd in cmds:
 		cmd = cmd.strip()
 		if len(cmd) == 0:
 			# empty command
 			continue
+		params['shell'] = shell
+		params['data'] = data
+		params['story'] = story
 		result = eval(preprocess(cmd), globals(), params)
 	return result
 
