710af405ab97f5045f6e357cee7407861e8d7748
DiscreteTom <discrete_tom@outlook.com>
2019-05-29 15:39:04 +0800

    update translator.py, add support for if/else

--- src/translator.py
+++ src/translator.py
@@ -1,10 +1,13 @@
+__if_flag = []
+
 def run(code: str, params = {}):
 	from shell import shell
 	from data import data
 	from story import story
 	if 'debug.run' in data.config:
 		print('debug.run: running', code, 'params:', params)
-	cmds = code.split(';')
+	cmds = code.replace('\n', ';').split(';')
+	result = None
 	for cmd in cmds:
 		cmd = cmd.strip()
 		if len(cmd) == 0:
@@ -13,7 +16,20 @@ def run(code: str, params = {}):
 		params['shell'] = shell
 		params['data'] = data
 		params['story'] = story
-		result = eval(preprocess(cmd), globals(), params)
+
+		# process if()/else/elif()/fi
+		if cmd.startswith('if(') and cmd[-1] == ')':
+			__if_flag.append(eval(cmd[3:-1]))
+		elif cmd == 'else':
+			__if_flag[-1] = not __if_flag[-1]
+		elif cmd == 'fi':
+			__if_flag.pop()
+		elif cmd.startswith('elif(') and cmd[-1] == ')':
+			__if_flag[-1] = eval(cmd[5:-1])
+		elif len(__if_flag) == 0 or __if_flag[-1]:
+			# run
+			result = eval(preprocess(cmd), globals(), params)
+	__if_flag.clear()
 	return result
 
 def preprocess(cmd: str):
