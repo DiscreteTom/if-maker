f8020f4e1dbb0f07bf7d664f79f13e494877ceb0
DiscreteTom <discrete_tom@outlook.com>
2019-06-10 16:10:40 +0800

    add debug command

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,14 @@
 from ifm import *
 if len(sys.argv) == 1:
-	print('usage:\nifm [new|make|run]')
+	print('usage:\nifm [new|make|run|debug]')
 if sys.argv[1] == 'new':
 	new()
 elif sys.argv[1] == 'make':
 	make()
 elif sys.argv[1] == 'run':
+	import controller
+	controller.start()
+elif sys.argv[1] == 'debug':
+	make()
 	import controller
 	controller.start()
