8f39f11d0b8784f4c5e59a999dfdee9c8d4e7ef8
DiscreteTom <discrete_tom@outlook.com>
2019-06-20 18:30:52 +0800

    update translator, add variable globalData

--- src/translator.py
+++ src/translator.py
@@ -1,21 +1,25 @@
+from shell import shell
+from data import data
+from story import story
+
+globalData = {}
+
 def run(code: str, params = {}):
-	from shell import shell
-	from data import data
-	from story import story
 	if 'debug.run' in data.config:
 		print('debug.run: running', code, 'params:', params)
 	result = None
 	# construct env
-	params['load'] = shell.load
-	params['unload'] = shell.unload
-	params['parse'] = shell.parse
-	params['print'] = story.print
-	params['printStory'] = story.printStory
-	params['printItemList'] = story.printItemList
-	params['config'] = data.config
-	params['items'] = data.items
-	params['game'] = data.game
-	params['findItem'] = data.findItem
-	params['run'] = run
-	result = exec(code, {}, params)
+	result = exec(code, globalData, params)
 	return result
+
+globalData['load'] = shell.load
+globalData['unload'] = shell.unload
+globalData['parse'] = shell.parse
+globalData['print'] = story.print
+globalData['printStory'] = story.printStory
+globalData['printItemList'] = story.printItemList
+globalData['config'] = data.config
+globalData['items'] = data.items
+globalData['game'] = data.game
+globalData['findItem'] = data.findItem
+globalData['run'] = run
