21821ca51b2dc3c92f068f72135c5813d0d24fa8
DiscreteTom <discrete_tom@outlook.com>
2019-07-01 09:34:12 +0800

    update controller.py, simplify start()

--- src/controller.py
+++ src/controller.py
@@ -11,30 +11,8 @@ readline.parse_and_bind('tab: complete')
 readline.set_completer(completer)
 
 def start():
-	# print welcome
-	if 'project.welcome' in data.config:
-		print(data.config['project.welcome'])
-	else:
-		print(data.config['project.name'])
-
-	# print main menu
-	for i in range(len(data.config['mainMenu'])):
-		print(i + 1, data.config['mainMenu'][i])
-
-	# get user's choice
-	index = 0
-	while True:
-		index = ord(msvcrt.getch()) - ord('0')
-		if index in range(1, len(data.config['mainMenu']) + 1):
-			break
-
-	# judge user input
-	if data.config['mainMenu'][index - 1] == 'new':
-		return newGame()
-	elif data.config['mainMenu'][index - 1] == 'continue':
-		return loop()
-	elif data.config['mainMenu'][index - 1] == 'exit':
-		return 0
+	from translator import run
+	run(data.config['system.entry'] + '()')
 
 def newGame():
 	story.printStory(data.config['system.story.first'])
