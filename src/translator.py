9c64db148cbfe82665b3b2e5721712a08c0f1cfc
DiscreteTom <discrete_tom@outlook.com>
2019-07-02 21:55:35 +0800

    update translator.py, can run scripts correctly

--- src/translator.py
+++ src/translator.py
@@ -1,12 +1,13 @@
 from shell import mount, unmount, parse, loadedItems
 from data import config, items, game, completer, findItem, save, load
-from story import print, printStory, printItemList
+from story import printf, printStory, printItemList
 from controller import start, newGame, loop
+from output import *
 
 def run(code: str, params = {}):
 	if 'debug.run' in config:
 		print('debug.run: running', code, 'params:', params)
 	result = None
 	# construct env
-	result = exec(code)
+	result = exec(code, globals(), params)
 	return result
