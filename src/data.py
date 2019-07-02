88bd475144b4fbdcbf98717eb483eb6ea269c613
DiscreteTom <discrete_tom@outlook.com>
2019-07-02 17:59:43 +0800

    update data.py, cancel class Data

--- src/data.py
+++ src/data.py
@@ -1,65 +1,73 @@
 '''
-- `data.config` to access config variables
-- `data.items` to access items
-- `data.game` to access global variables
-- `data.completer` to add user defined words
-- `data.findItem(itemName)` to get item id
-- `data.save/load(fileName)` to save/mount game progress
+- `config` to access config variables
+- `items` to access items
+- `game` to access global variables
+- `completer` to add user defined words
+- `findItem(itemName)` to get item id
+- `save/load(fileName)` to save/mount game progress
 '''
 
 from refdict import refdict
 import json
 
-class Data:
-	def __init__(self):
-		# load config
-		f = open('output/config', encoding='utf-8')
-		self.config = refdict(json.load(f))
-		f.close()
-		# load items
-		f = open('output/items', encoding='utf-8')
-		self.items = refdict(json.load(f))
-		f.close()
-		# user defined global data
-		self.game = refdict({})
-		self.completer = set()
-	def findItem(self, itemName: str):
-		'''
-		return itemID
-		'''
-		for itemID in self.items:
-			if self.items[itemID]["name"] == itemName:
-				return itemID
-		return None
-	
-	def save(self, fileName: str):
-		'''
-		save game progress to `fileName`
-		'''
-		from shell import shell
-		f = open(fileName, 'w', encoding='utf-8')
-		result = {
-			'items': str(self.items),
-			'config': str(self.config),
-			'game': str(self.game),
-			'completer': str(self.completer),
-			'shell': str(shell.data())
-		}
-		json.dump(result, f)
-		f.close()
-	
-	def load(self, fileName: str):
-		'''
-		load game progress from `fileName`
-		'''
-		from shell import shell
+config = refdict({})
+items = refdict({})
+game = refdict({})
+completer = set()
+
+def findItem(itemName: str):
+	'''
+	return item ID. if item ID is not found, return None
+	'''
+	for itemID in items:
+		if items[itemID]["name"] == itemName:
+			return itemID
+	return None
+
+def save(fileName: str):
+	'''
+	save game progress to `fileName`
+	'''
+	import shell
+	f = open(fileName, 'w', encoding='utf-8')
+	result = {
+		'items': str(items),
+		'config': str(config),
+		'game': str(game),
+		'completer': str(completer),
+		'shell': str(shell.loadedItems)
+	}
+	json.dump(result, f)
+	f.close()
+
+def load(fileName = '') -> None:
+	'''
+	load game progress from `fileName`
+
+	if `fileName` is not given, load data from default location
+	'''
+	import shell
+	if len(fileName):
+		# load data from SAVE file
 		f = open(fileName, encoding='utf-8')
 		d = json.load(f)
-		self.completer = eval(d['completer'])
-		self.items = eval(d['items'])
-		self.config = eval(d['config'])
-		self.game = eval(d['game'])
-		shell.load(eval(d['shell']))
+		completer = eval(d['completer'])
+		items = eval(d['items'])
+		config = eval(d['config'])
+		game = eval(d['game'])
+		shell.loadedItems = eval(d['shell'])
 		f.close()
+		return
 
-data = Data()
+	# ======================== load file from default location
+	# load config
+	f = open('output/config', encoding='utf-8')
+	config = refdict(json.load(f))
+	f.close()
+	# load items
+	f = open('output/items', encoding='utf-8')
+	items = refdict(json.load(f))
+	f.close()
+	# user defined global data
+	game = refdict({})
+	completer = set()
