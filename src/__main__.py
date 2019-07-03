5e475e9b60a423b5a17feb6edcb02cd3b8ddcc38
DiscreteTom <discrete_tom@outlook.com>
2019-07-03 15:07:29 +0800

    update __main__.py, optimize help msg

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,23 @@
 from ifm import *
 if len(sys.argv) == 1:
-	print('usage:\nifm [new|make|run|debug]')
-if sys.argv[1] == 'new':
+	print('usage: ifm {new|make|run|debug|package} [projectName]\n')
+	print('create a new project:')
+	print('	ifm new [projectName]')
+	print('')
+	print('make the project in this folder:')
+	print('	ifm make')
+	print('')
+	print('run your project:')
+	print('	ifm run')
+	print('')
+	print('make and run your project:')
+	print('	ifm debug')
+	print('')
+	print('package your project to an executable file:')
+	print('	ifm package')
+elif sys.argv[1] == 'new':
+	if len(sys.argv) > 2:
+		new(sys.argv[2])
 	new()
 elif sys.argv[1] == 'make':
 	make()
@@ -12,3 +28,5 @@ elif sys.argv[1] == 'debug':
 	make()
 	import controller
 	controller.start()
+elif sys.argv[1] == 'package':
+	pass
